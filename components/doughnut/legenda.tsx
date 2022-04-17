7e8bbf7d41227bde821d3a52aedb9fe0c4b0797e
AldenorNeto <74365018+AldenorNeto@users.noreply.github.com>
2022-04-16 22:33:42 -0300

    separando repositorios

--- components/doughnut/legenda.tsx
+++
@@ -1,7 +0,0 @@
-export default function legendaDoughnut(props:any): JSX.Element{ 
-return(
-<div style={{display: 'flex', width: '98%'}}>
-    {props.label.setor.map((propriedade:any, indice: any)=>(<section style={{display:'flex',alignItems:'center',fontSize:'10px'}}><div className="legendaDoug" style={{backgroundColor: props.label.cor[indice]}}></div><span>{propriedade}</span></section>))}
-</div>
-)
-}
--- components/espaco/espaco.tsx
+++
@@ -1,3 +0,0 @@
-export default function space(): JSX.Element{
-return(<div className='spaceBetween' style={{width:'10px',height:'10px'}}></div>)
-}
