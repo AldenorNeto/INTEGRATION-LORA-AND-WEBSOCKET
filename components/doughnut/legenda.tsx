5c8f0949440e6ea0705c05b189c510ff95a1f4c5
Aldenorneto <arrudaneto@grendene.com.br>
2022-04-19 09:49:52 -0300

    reajustando arquivos do repositorio

--- components/doughnut/legenda.tsx
+++
@@ -1,8 +0,0 @@
-import style from './doughnutCosumoFluido.module.scss'
-export default function legendaDoughnut(props:any): JSX.Element{ 
-return(
-<div style={{display: 'flex', width: '98%', padding:'10px'}}>
-    {props.label.setor.map((propriedade:any, indice: any)=>(<section style={{display:'flex',alignItems:'center',fontSize:'10px'}}><div className={style.legendaDoug} style={{backgroundColor: props.label.cor[indice]}}></div><span>{propriedade}</span></section>))}
-</div>
-)
-}
